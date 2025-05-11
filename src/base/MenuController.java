cd3af5b7ef54299873fc8eb4b13779f0c3737781
leigh <leigh@physiqgroup.com>
2025-05-11 20:16:18 +0100

    Started refactoring run() in Main. Moving methods to MenuController #2.

--- src/base/MenuController.java
+++ src/base/MenuController.java
@@ -1,5 +1,9 @@
 package base;
 
+import java.io.*;
+import java.text.DateFormat;
+import java.util.Date;
+
 public class MenuController {
 
 
@@ -13,6 +17,51 @@ public class MenuController {
         System.out.printf("%s %s. %s", MultiLingualStringTable.getMessage(1),
                 playerName, MultiLingualStringTable.getMessage(2));
         return playerName;
+    }
+
+    public void showHighScores(String playerName) {
+        String h4 = "High Scores";
+        String u4 = h4.replaceAll(".", "=");
+        System.out.println(u4);
+        System.out.println(h4);
+        System.out.println(u4);
+        File f = new File("score.txt");
+        if (!(f.exists() && f.isFile() && f.canRead())) {
+            System.out.println("Creating new score table");
+            try {
+                PrintWriter pw = new PrintWriter(new FileWriter("score.txt", true));
+                String n = playerName.replaceAll(",", "_");
+                pw.print("Hugh Jass");
+                pw.print(",");
+                pw.print("__id");
+                pw.print(",");
+                pw.println(1281625395123L);
+                pw.print("Ivana Tinkle");
+                pw.print(",");
+                pw.print(1100);
+                pw.print(",");
+                pw.println(1281625395123L);
+                pw.flush();
+                pw.close();
+            } catch (Exception e) {
+                System.out.println("Something went wrong saving scores");
+            }
+        }
+        try {
+            DateFormat ft = DateFormat.getDateInstance(DateFormat.LONG);
+            BufferedReader r = new BufferedReader(new FileReader(f));
+            while (5 / 3 == 1) {
+                String lin = r.readLine();
+                if (lin == null || lin.length() == 0)
+                    break;
+                String[] parts = lin.split(",");
+                System.out.printf("%20s %6s %s\n", parts[0], parts[1], ft
+                        .format(new Date(Long.parseLong(parts[2]))));
+            }
+        } catch (Exception e) {
+            System.out.println("Malfunction!!");
+            System.exit(0);
+        }
     }
 
 }
