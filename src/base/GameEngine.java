2c29d4c44c5f50ccc48158c95e65f3bf2406175a
leigh <leigh@physiqgroup.com>
2025-05-12 16:46:25 +0100

    Refactored - moved domino creation to new class DominoFactory to apply factory pattern.

--- src/base/GameEngine.java
+++ src/base/GameEngine.java
@@ -38,38 +38,6 @@ public class GameEngine {
     }
 
 
-
-    // Create a list of Dominoes.
-    public List<Domino> generateDominoList(boolean shouldPlace) {
-        List<Domino> list = new LinkedList<>();
-        int count = 0;
-        int x = 0;
-        int y = 0;
-
-        for (int l = 0; l <= MAX_DOMINO_SPOTS; l++) {
-            for (int h = l; h <= MAX_DOMINO_SPOTS; h++) {
-                Domino d = new Domino(h, l);
-                if (shouldPlace) {
-                    d.place(x, y, x + 1, y);
-                    x += 2;
-                    if (x > 6) {
-                        x = 0;
-                        y++;
-                    }
-                }
-                list.add(d);
-                count++;
-            }
-        }
-
-        if (count != TOTAL_DOMINOS) {
-            System.out.println("Something went wrong generating dominoes");
-            System.exit(0);
-        }
-
-        return list;
-    }
-
     public void generateDominoes() {
         dominoList = DominoFactory.generateDominoList(true);
     }
