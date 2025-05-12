022543d1e99f3d13ddfaacae02c66f25c938a90c
leigh <leigh@physiqgroup.com>
2025-05-12 19:31:34 +0100

    JUnit Testing - DominoFactoryTest.

--- test/base/DominoFactoryTest.java
+++ test/base/DominoFactoryTest.java
@@ -1,4 +1,21 @@
 package base;
 
-public class DominoFactoryTest {
+import org.junit.jupiter.api.Test;
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DominoFactoryTest {
+
+    @Test
+    void testDominoListSize_shouldAlwaysBe28() {
+        List<Domino> dominoes = DominoFactory.generateDominoList(true);
+        assertEquals(28, dominoes.size());
+
+        dominoes = DominoFactory.generateDominoList(false);
+        assertEquals(28, dominoes.size());
+    }
+
 }
