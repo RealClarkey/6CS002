8d0d713621cc728e1b06a1cbfa354a7312ec9f68
leigh <leigh@physiqgroup.com>
2025-05-12 19:54:38 +0100

    JUnit Testing - DominoFactoryTest Complete.

--- test/base/DominoFactoryTest.java
+++ test/base/DominoFactoryTest.java
@@ -10,12 +10,41 @@ import static org.junit.jupiter.api.Assertions.*;
 class DominoFactoryTest {
 
     @Test
-    void testDominoListSize_shouldAlwaysBe28() {
+    // Test if all dominoes are being generated.
+    void testDominoListSizeIs28() {
         List<Domino> dominoes = DominoFactory.generateDominoList(true);
         assertEquals(28, dominoes.size());
 
         dominoes = DominoFactory.generateDominoList(false);
         assertEquals(28, dominoes.size());
+    }
+
+    @Test
+    // Test all Dominoes are placed when shouldPlace = true
+    void testDominoesArePlacedShouldPlaceTrue() {
+        List<Domino> dominoes = DominoFactory.generateDominoList(true);
+        assertTrue(dominoes.stream().allMatch(d -> d.placed));
+    }
+
+    @Test
+    // Test no dominoes are place when shouldPlace = false
+    void testDominoesNotPlacedShouldPlaceFalse() {
+        List<Domino> dominoes = DominoFactory.generateDominoList(false);
+        assertTrue(dominoes.stream().noneMatch(d -> d.placed));
+    }
+
+    @Test
+    // Test that all Dominoes are Unique.
+    void testAllDominoesAreUnique() {
+        List<Domino> dominoes = DominoFactory.generateDominoList(false);
+        Set<String> pairs = new HashSet<>();
+
+        for (Domino d : dominoes) {
+            String pair = d.high + "," + d.low;
+            String reversePair = d.low + "," + d.high;
+            assertFalse(pairs.contains(pair) || pairs.contains(reversePair));
+            pairs.add(pair);
+        }
     }
 
 }
--- test/base/GameEngineTest.java
+++ test/base/GameEngineTest.java
@@ -1,4 +1,18 @@
 package base;
+import org.junit.jupiter.api.Test;
 
-public class GameEngineTest {
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameEngineTest {
+    @Test
+        // Test if all dominoes are being generated.
+    void testDominoListSizeIs28() {
+        List<Domino> dominoes = DominoFactory.generateDominoList(true);
+        assertEquals(28, dominoes.size());
+
+        dominoes = DominoFactory.generateDominoList(false);
+        assertEquals(28, dominoes.size());
+    }
 }
